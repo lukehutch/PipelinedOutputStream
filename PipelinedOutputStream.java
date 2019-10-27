98f042728a50d61ed4517ab27e20a9be14f857a2
Luke Hutchison <luke.hutch@gmail.com>
2019-10-26 23:05:38 -0600

    Remove package name

--- PipelinedOutputStream.java
+++ PipelinedOutputStream.java
@@ -26,8 +26,6 @@
  * AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
  * OR OTHER DEALINGS IN THE SOFTWARE.
  */
-package slam.utils;
-
 import java.io.BufferedOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
